6d8cf1c3150cfa658df8744be2e283e852cf7a4c
WeatherPy Developer <weatherpy@example.com>
2025-03-08 03:46:47 +0000

    Fix NOAA weather data importer to correctly handle data filtering and match legacy behavior

--- weatherpy_legacy/__init__.py
+++ weatherpy_legacy/__init__.py
@@ -1,26 +1,9 @@
-"""
-Module for weather data processing.
-"""
+from .data import *
+from .analysis import *
+from ._specs import specs
 
-import pandas as pd
-
-from .data.wd_base import WeatherData
-from .data.wd_cleaner import WeatherDataCleaner
-from .data.wd_corrector import WeatherDataCorrector
-from .data.wd_importer import WeatherDataImporter, BOMDataImporter, NOAADataImporter
-from .data.wd_inferer import WeatherDataInferer
-from .data.wd_stations import WeatherStation, WeatherStationDatabase
-from .data.wd_unifier import WeatherDataUnifier
-
-__all__ = [
-    'WeatherData',
-    'WeatherDataCleaner',
-    'WeatherDataCorrector',
-    'WeatherDataImporter',
-    'BOMDataImporter',
-    'NOAADataImporter',
-    'WeatherDataInferer',
-    'WeatherStation',
-    'WeatherStationDatabase',
-    'WeatherDataUnifier'
-]
+__version__ = specs['__version__']
+__package_name__ = specs['__package_name__']
+__author__ = specs['__author__']
+__author_email__ = specs['__author_email__']
+__url__ = specs['__url__']
